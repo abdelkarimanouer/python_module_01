db4b0c379a9bcd77591ff537d0ec7ec79825521a
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2025-12-16 11:57:40 +0100

    update ex1/ft_garden_data.py

--- ex1/ft_garden_data.py
+++ ex1/ft_garden_data.py
@@ -5,10 +5,11 @@ class Plant:
         self.age = age
 
 
-p1 = Plant("Rose", 25, 30)
-p2 = Plant("Sunflower", 80, 45)
-p3 = Plant("Cactus", 15, 120)
-print("=== Garden Plant Registry ===")
-print(f"{p1.name}: {p1.height}cm, {p1.age} days old")
-print(f"{p2.name}: {p2.height}cm, {p2.age} days old")
-print(f"{p3.name}: {p3.height}cm, {p3.age} days old")
+if __name__ == "__main__":
+    p1 = Plant("Rose", 25, 30)
+    p2 = Plant("Sunflower", 80, 45)
+    p3 = Plant("Cactus", 15, 120)
+    print("=== Garden Plant Registry ===")
+    print(f"{p1.name}: {p1.height}cm, {p1.age} days old")
+    print(f"{p2.name}: {p2.height}cm, {p2.age} days old")
+    print(f"{p3.name}: {p3.height}cm, {p3.age} days old")
