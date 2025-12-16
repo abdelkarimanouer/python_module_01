d2aa37dcad49187826066c9a62058ecaa3dce00c
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2025-12-16 09:02:52 +0100

    update ex0/ft_garden_intro

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    plant = "Rose"
+    height = 25
+    age = 30
+    print("=== Welcome to My Garden ===")
+    print("Plant: " + plant)
+    print("Height: " + str(height) + "cm")
+    print("Age: " + str(age) + " days")
+    print("\n=== End of Program ===")
