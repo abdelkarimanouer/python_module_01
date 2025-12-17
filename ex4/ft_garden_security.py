c90337f9a883768d149b2b82381db960114e5111
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2025-12-17 10:24:12 +0100

    add docstring to file

--- ex4/ft_garden_security.py
+++ ex4/ft_garden_security.py
@@ -1,11 +1,17 @@
+"""This is a SecurePlant class that protects data from bad values"""
+
+
 class SecurePlant:
+    """Blueprint for making secure plant objects with validation"""
     def __init__(self, name) -> None:
+        """Setup plant with name and empty height and age"""
         self.name = name
         self.__height: int
         self.__age: int
         print(f"Plant created: {self.name}")
 
     def set_height(self, height: int) -> None:
+        """Set plant height if value is valid"""
         if (height >= 0):
             self.__height = height
             print(f"Height updated: {self.__height}cm [OK]")
@@ -16,11 +22,13 @@ class SecurePlant:
 
     def get_height(self) -> int:
         if (self.__height >= 0):
+            """Get plant height safely"""
             return (self.__height)
         else:
             return 0
 
     def set_age(self, age: int) -> None:
+        """Set plant age if value is valid"""
         if (age >= 0):
             self.__age = age
             print(f"Age updated: {self.__age} days [OK]")
@@ -30,6 +38,7 @@ class SecurePlant:
             print("Security: Negative age rejected")
 
     def get_age(self) -> int:
+        """Get plant age safely"""
         if (self.__age >= 0):
             return (self.__age)
         else:
@@ -37,6 +46,8 @@ class SecurePlant:
 
 
 if __name__ == "__main__":
+    """Main program starts here"""
+
     print("=== Garden Security System ===")
     p1 = SecurePlant("Rose")
     p1.set_height(25)
